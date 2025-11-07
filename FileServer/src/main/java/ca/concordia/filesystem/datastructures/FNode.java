d6c890d0c41d8f9425f32033a2ab177f83fe6d3c
Zakaria El fettahi <zakariaelfettahi@gmail.com>
2025-11-07 18:01:59 -0500

    Added getters and setters to Fnode file

--- FileServer/src/main/java/ca/concordia/filesystem/datastructures/FNode.java
+++ FileServer/src/main/java/ca/concordia/filesystem/datastructures/FNode.java
@@ -10,4 +10,28 @@ public class FNode {
         this.blockIndex = blockIndex;
         this.next = -1;
     }
+    
+    // Getters 
+
+    //getNext for traversing the linked list of blocks
+    public int getNext() {
+        return next;
+    }
+
+    //getBlock to know where on disk to read from 
+    public int getBlockIndex() {
+        return blockIndex;
+    }
+
+    // Setters
+
+    //setBlockIndex to track which blocks are available (free or used)
+    public void setBlockIndex(int blockIndex) {
+        this.blockIndex = blockIndex;
+    }
+
+    // setNext to link blocks together
+    public void setNext(int next) {
+        this.next = next;
+    }
 }
