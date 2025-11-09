6dee518e4afbab09861a125c1e3543184c99ae0e
L <abolished>
2025-11-09 16:55:02 -0500

    added first draft of operations: Create file, Delete file, List files

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -35,12 +35,65 @@ public class FileSystemManager {
 
     }
 
-    public void createFile(String fileName) throws Exception {
-        // TODO
-        //throw new UnsupportedOperationException("Method not implemented yet.");
-        //FEntry newFile = new FEntry(fileName, (short) 0, (short) -1);
-
+    public void createFile(String fileName){
+        int freeNodeIndex = findFreeInode();
+        if (freeNodeIndex == -1){
+            throw new IllegalStateException("No free filespace.");
+        }
+        if (findFileIndex(fileName) != -1){  //otherwise returns index of file if found with name fileName
+            throw new IllegalArgumentException("File already exists: " + fileName);
+        }
+        FEntry newFile = new FEntry(fileName, (short) 0, (short) -1); //filesize 0, no blocks allocated (signalled by -1)
+        this.inodeTable[freeNodeIndex] = newFile;
     }
+
+    public void deleteFile(String fileName){
+        if (fileName == null){
+            throw new IllegalArgumentException("Filename cannot be empty.")
+        };
+        fileIndex = findFileIndex(fileName);
+        if (fileIndex == -1){
+            throw new IllegalArgumentException("File not found: " + fileName);
+        };
+        FEntry fileToDelete = inodeTable[fileIndex];
+        freeBlockList[fileToDelete.getFirstBlock()] = true; //frees block corresponding to file-to-be-deleted's first block
+        inodeTable[fileIndex] = null;
+    }
+
+    String[] listFiles(){
+        String[] fileList = new String[countFiles()];   //returns array of size = # of non-null entries
+        for (int i = 0; i < MAXFILES; i++){
+            if (inodeTable[i] != null){
+                fileList[i] = inodeTable[i].getFilename();
+            };
+        }
+        return fileList;
+    }
+
+    private int findFreeInode(){
+        for (int i = 0; i < MAXFILES; i++){
+            if (inodeTable[i] == null) return i;
+        }
+        return -1;
+    }
+
+    private int findFileIndex(String fileName){
+        for (int i = 0; i < MAXFILES; i++){
+            if (inodeTable[i] != null && inodeTable[i].getFilename().equals(fileName)){
+                return i;
+            };
+        }
+        return -1;
+    }
+
+    private int countFiles(){
+        count = 0;
+        for (int i = 0; i < MAXFILES; i++){
+            if (inodeTable[i] != null) count++;
+        }
+        return count;
+    }
+
 
 
     // TODO: Add readFile, writeFile and other required methods,
