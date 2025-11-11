ccfd1728e8da93c23e64c9dd62c7482dde2037dd
Chandler9803 <chaoran.chen98@gmail.com>
2025-11-10 20:59:04 -0500

    create file

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -46,8 +46,31 @@ public class FileSystemManager {
     // create file
     public void createFile(String fileName) throws Exception {
         // TODO
-        throw new UnsupportedOperationException("Method not implemented yet.");
+        globalLock.lock();
+        try {
+            if (fileName.length() > 11) {
+                throw new Exception("ERROR: filename too long");
+            }
+            // check if file already exists
+            for (FEntry e : inodeTable) {
+                if (e.getFilename().equals(fileName)) {
+                    throw new Exception("ERROR: File already exists.");
+                }
+            }
+            // find a free slot
+            for (int i = 0; i < MAXFILES; i++) {
+                if (inodeTable[i].getFilename().isEmpty()) {
+                    inodeTable[i].setFilename(fileName);
+                    inodeTable[i].setFilesize((short) 0);
+                    inodeTable[i].setFirstBlock((short) -1);
+                    return;
+                }
+            }
+            throw new Exception("ERROR: no space for new file");
+        } finally {
+            globalLock.unlock();
+        }
     }
 
-    // TODO: Add readFile, writeFile and other required methods,
+// TODO: Add readFile, writeFile and other required methods,
 }
