62e4ee0e1bd5dc38a1fbe8c9bdfb2df753b6fcf9
Chandler9803 <chaoran.chen98@gmail.com>
2025-11-10 20:17:06 -0500

    constructor

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -1,15 +1,15 @@
 package ca.concordia.filesystem;
 
-import ca.concordia.filesystem.datastructures.FEntry;
-
 import java.io.RandomAccessFile;
 import java.util.concurrent.locks.ReentrantLock;
+
+import ca.concordia.filesystem.datastructures.FEntry;
 
 public class FileSystemManager {
 
     private final int MAXFILES = 5;
     private final int MAXBLOCKS = 10;
-    private final static FileSystemManager instance;
+    private static FileSystemManager instance;
     private final RandomAccessFile disk;
     private final ReentrantLock globalLock = new ReentrantLock();
 
@@ -18,21 +18,36 @@ public class FileSystemManager {
     private FEntry[] inodeTable; // Array of inodes
     private boolean[] freeBlockList; // Bitmap for free blocks
 
+    // constructor
     public FileSystemManager(String filename, int totalSize) {
         // Initialize the file system manager with a file
-        if(instance == null) {
-            //TODO Initialize the file system
+        if (instance == null) {
+            instance = this;
+            inodeTable = new FEntry[MAXFILES];
+            for (int i = 0; i < MAXFILES; i++) {
+                inodeTable[i] = new FEntry("", (short) 0, (short) -1);
+            }
+            freeBlockList = new boolean[MAXBLOCKS];
+            for (int i = 0; i < MAXBLOCKS; i++) {
+                freeBlockList[i] = true;
+            }
+            try {
+                disk = new RandomAccessFile(filename, "rw");
+                disk.setLength(totalSize);
+            } catch (Exception e) {
+                throw new RuntimeException("Unable to create virtual disk file", e);
+            }
         } else {
             throw new IllegalStateException("FileSystemManager is already initialized.");
         }
 
     }
 
+    // create file
     public void createFile(String fileName) throws Exception {
         // TODO
         throw new UnsupportedOperationException("Method not implemented yet.");
     }
-
 
     // TODO: Add readFile, writeFile and other required methods,
 }
