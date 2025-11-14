11a2e14b1d17823e1efcb6d0c834bc230e998ab9
N Chittoo <chittoonavnit@gmail.com>
2025-11-13 19:54:12 -0500

    updated FileServer.java & Main.java

--- FileServer/src/main/java/ca/concordia/Main.java
+++ FileServer/src/main/java/ca/concordia/Main.java
@@ -13,100 +13,26 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.printf("Hello and welcome!");
-
-        FileServer server = new FileServer(12345, "filesystem.dat", 10 * 128);
+        System.out.print("Hello and welcome!");
 
         // totalSize = metadataSize + (MAXBLOCKS × BLOCKSIZE)
+        // MAXBLOCKS = 10
+        // BLOCKSIZE = 128
 
-        String file_entry = args[0]; // command user enter
-        int totalsize = Integer.parseInt(args[1]);
-
-        new Thread(() -> {
-            try {
-                server.start();
-                // Start the file server
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
-
-        try{
-            FileSystemManager fsm = new FileSystemManager(file_entry, totalsize);
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            while(true){
-                System.out.println("ENTER COMMANDS >>");
-                String line = br.readLine();
-                if(line == null){
-                    System.out.println("COMMAND NOT DETECTED.");
-                    break;
-                }
-                else{
-                    String[] parts = line.split("\\s+",3);
-                    String cmd = parts[0].toUpperCase();
-
-                    try{
-                        switch (cmd){
-                            case "CREATE": {
-                                String filename = parts[1];
-                                if(!isNameValid(filename)){
-                                    break;
-                                }
-                                fsm.createFile(filename);
-                                System.out.println("File created successfully");
-                                break;
-                            }
-                            case "WRITE": {
-                                String filename = parts[1];
-                                if(!isNameValid(filename)){
-                                    break;
-                                }
-                                String content = parts[2];
-                                byte[] data = content.getBytes();
-                                fsm.writeFile(filename,data);
-                                System.out.println("Wrote " + data.length + " bytes");
-                            }
-                            case "READ": {
-                                String filename = parts[1];
-                                if(!isNameValid(filename)){
-                                    break;
-                                }
-                                byte[] data = fsm.readFile(filename);
-                                String filetext = new String(data);
-                                System.out.println("File Contents: " + filetext);
-                            }
-                            case "DELETE": {
-                                String filename = parts[1];
-                                if(!isNameValid(filename)){
-                                    break;
-                                }
-                                fsm.deleteFile(filename);
-                                System.out.println("File " + filename + " successfully deleted");
-                            }
-                            case "LIST": {
-                                List<String> names = Arrays.asList(fsm.listFiles());
-                            }
-                        }
-                    } catch (Exception e) {
-                        System.out.println("Incorrect command entered");
-                        throw new RuntimeException(e);
-                    }
-                }
-
-            }
-        } catch (Exception e) { // exception handling
-            throw new RuntimeException(e);
+        if (args.length != 3){
+            System.out.println("ERROR IN SERVER parameters- <port> <filename> <totalSize> ");
+            return;
         }
-    }
 
-    // Modularity
-    private static boolean isNameValid(String name){
-        boolean flag = true;
-        if(name.length() > 11){
-            System.out.println("Name of file should be less than 11 characters");
-            flag = false;
-        }
-        return flag;
+        int port = Integer.parseInt(args[0]);
+        String filename = args[1]; // filename
+        int totalsize = Integer.parseInt(args[2]);
+
+        // starting server
+        FileServer server = new FileServer(port, filename, totalsize);
+        server.start();
+
+
+
     }
 }
